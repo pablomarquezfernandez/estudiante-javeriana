3b645090462e851953abeb741f8edf5a3a24cc2c
pablomarquezfernandez <124263202+pablomarquezfernandez@users.noreply.github.com>
2024-04-09 16:45:55 -0500

    información

--- src/test/java/co/gov/javeriana/estudiante/estudiantes/EstudianteTest.java
+++ src/test/java/co/gov/javeriana/estudiante/estudiantes/EstudianteTest.java
@@ -1,29 +1,51 @@
 package co.gov.javeriana.estudiante.estudiantes;
 
+
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import co.gov.javeriana.estudiante.estudiantes.controllers.EstudianteController;
 import co.gov.javeriana.estudiante.estudiantes.dto.EstudianteDTO;
 import co.gov.javeriana.estudiante.estudiantes.services.EstudianteService;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class EstudianteTest {
+class EstudianteTest {
 
     @Autowired
 	EstudianteService estudianteController;
 
 	@Test
-	public void pruebaEstudiante() {
+	void estudianteTest() {
 
-		
+		System.out.println("----------------------");
+		System.out.println("----------------------");
+		System.out.println("----------------------");
+		System.out.println("----------------------");
+		System.out.println("----------------------");
+
+		int cantidad = estudianteController.get().size();
+		EstudianteDTO estudianteDTO = new EstudianteDTO(null, "Pablo", "Márquez", "pablo.marquez@dynaco.co");
+		estudianteDTO = estudianteController.save(estudianteDTO);
+		int nuevaCantidad = estudianteController.get().size();
+		assertEquals(cantidad + 1, nuevaCantidad);
+
+		System.out.println("----------------------");
+		System.out.println("----------------------");
+		System.out.println("----------------------");
+		System.out.println("----------------------");
+		estudianteDTO.setNombres("Pablo Andres");
+		estudianteController.update(estudianteDTO);
+		EstudianteDTO estudianteActualizadoDTO = estudianteController.get(estudianteDTO.getId());
+		assertEquals("Pablo Andres", estudianteActualizadoDTO.getNombres() );
+
+		System.out.println("----------------------");
+		System.out.println("----------------------");
+		System.out.println("----------------------");
 
 	}
 }
